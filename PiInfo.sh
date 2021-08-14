d85cb012f7121a2236e2f90054534848382a302d
DaLang <dalang83@gmail.com>
2021-08-13 21:45:26 -0400

    First Commit for Basic Bash Script Attempt

---
+++ PiInfo.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#Script for Outputting to CLI Info about the current stats of the PiHole Home Server. Will list the Active Daemons, Disk Stats,
+#a Current Speedtest and Pihole status
+
+
+#Introduction followed by outputs with clearing of proceeding information.
+clear;
+	echo -e "$(tput setaf 1)Welcome to the PiHomeServer Status Script $USER: $(tput sgr 0) \n"
+		sleep 2
+
+	echo -e "$(tput setaf 1)Active Internet Connections: $(tput sgr 0) \n"
+
+		sudo netstat -nultp | awk 'NR>2' | cut -d "/" -f2 | cut -d ' ' -f 1 | sed 's/://' | sort -u | sed 's/\(.*\)/  \1/'
+		sleep 2
+	echo
+
+	echo -e "$(tput setaf 1)Disk Statistics: $(tput sgr 0) \n";
+
+			sudo iostat -h | sed 's/\(.*\)/  \1/'
+		sleep 2
+			sudo lsblk | sed 's/\(.*\)/  \1/'
+		sleep 2
+	echo
+
+	echo -e "$(tput setaf 1)PiHole Status: $(tput sgr 0) \n"
+
+		pihole status
+	echo
+	echo -e "$(tput setaf 1)Unbound Status: $(tput sgr 0) \n";
+
+		systemctl status unbound | grep 'Active:'
+		sleep 2
+	echo
+
+	echo -e "$(tput setaf 1)Running a speedtest please be patient ... $(tput sgr 0) \n"
+			speedtest-cli --simple | sed 's/\(.*\)/  \1/'
+		sleep 2
+	echo
+
+	echo -e "$(tput setaf 1)Process complete ... $(tput sgr 0)"
+		sleep 2
+
+
+
