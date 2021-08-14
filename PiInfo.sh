9def14551d1f334b2145a93939cfb9b563eb49aa
DaLang <dalang83@gmail.com>
2021-08-14 01:36:19 -0400

    Some minor corrections and adjustments

--- PiInfo.sh
+++ PiInfo.sh
@@ -4,20 +4,20 @@
 
 
 #Introduction followed by outputs with clearing of proceeding information.
+
 clear;
 	echo -e "$(tput setaf 1)Welcome to the PiHomeServer Status Script $USER: $(tput sgr 0) \n"
-		sleep 2
 
-	echo -e "$(tput setaf 1)Active Internet Connections: $(tput sgr 0) \n"
+	echo -e "$(tput setaf 1)Active Web Daemons: $(tput sgr 0) \n"
 
 		sudo netstat -nultp | awk 'NR>2' | cut -d "/" -f2 | cut -d ' ' -f 1 | sed 's/://' | sort -u | sed 's/\(.*\)/  \1/'
 		sleep 2
 	echo
 
-	echo -e "$(tput setaf 1)Disk Statistics: $(tput sgr 0) \n";
+	echo -e "$(tput setaf 1)Disk Statistics: $(tput sgr 0) \n"
 
 			sudo iostat -h | sed 's/\(.*\)/  \1/'
-		sleep 2
+
 			sudo lsblk | sed 's/\(.*\)/  \1/'
 		sleep 2
 	echo
@@ -26,19 +26,23 @@ clear;
 
 		pihole status
 	echo
-	echo -e "$(tput setaf 1)Unbound Status: $(tput sgr 0) \n";
+		sleep 2
+
+	echo -e "$(tput setaf 1)Unbound Status: $(tput sgr 0) \n"
 
 		systemctl status unbound | grep 'Active:'
 		sleep 2
 	echo
 
 	echo -e "$(tput setaf 1)Running a speedtest please be patient ... $(tput sgr 0) \n"
+
 			speedtest-cli --simple | sed 's/\(.*\)/  \1/'
 		sleep 2
 	echo
 
 	echo -e "$(tput setaf 1)Process complete ... $(tput sgr 0)"
-		sleep 2
+
+		sleep 1
 
 
 
